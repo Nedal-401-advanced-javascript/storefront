5c583b967aae36d988845fe813b5af67ed3e71ad
Nedal_Erekat <nedalerekat@gmail.com>
2020-10-27 22:33:22 +0300

    add delete functionality

--- storefront/src/store/cart.js
+++ storefront/src/store/cart.js
@@ -3,7 +3,13 @@ let cart = [];
 export default (state = cart, action) => {
   switch (action.type) {
     case "addToCart":
-      return [...state, action.payload.name];
+      return [...state, action.payload];
+    case "del":
+      // should create module for cart 
+      // should use superagent to delete from cart database(delete)
+      state.splice(action.payload,1)
+      console.log(state);
+      return [...state]
 
     default:
       return state;
@@ -11,8 +17,15 @@ export default (state = cart, action) => {
 };
 
 export const updateCart = (product) => {
+  // should use superagent to update cart database module(put)
   return {
     type: "addToCart",
     payload: product,
+  };
+};
+export const delItem = (itemId) => {
+  return {
+    type: "del",
+    payload: itemId,
   };
 };
