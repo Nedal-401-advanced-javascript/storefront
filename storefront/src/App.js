a2fdf99b82f7aa59640da368b58aae6975c5373d
Nedal_Erekat <nedalerekat@gmail.com>
2020-10-27 20:01:42 +0300

    useEffect() to dispatch on the initial page load

--- storefront/src/App.js
+++ storefront/src/App.js
@@ -1,16 +1,17 @@
 import Header from "./component/header";
 import Footer from "./component/footer";
-import Categories from './component/categories/categories'
-import Products from "./component/products/products"
+import Categories from "./component/categories/categories";
+import Products from "./component/products/products";
 import SimpleCart from "./component/simpleCart/index";
+import superagent from "superagent";
 
 function App() {
   return (
     <>
       <Header />
       <SimpleCart />
-      <Categories/>
-      <Products/>
+      <Categories />
+      <Products />
       <Footer />
     </>
   );
--- storefront/src/store/index.js
+++ storefront/src/store/index.js
@@ -1,7 +1,8 @@
-import { createStore, combineReducers } from "redux";
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
 import categories from "./categories";
 import products from "./products";
 import cart from "./cart"
 let reducers = combineReducers({ categories,products,cart });
-const store = () => createStore(reducers);
+const store = () => createStore(reducers,applyMiddleware(thunk));
 export default store();
