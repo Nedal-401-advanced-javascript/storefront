53cb717366dcc7e8347a0049c533c9360bacf499
Nedal_Erekat <nedalerekat@gmail.com>
2020-10-29 22:52:56 +0300

    uncompleted working on the lab RTK

--- storefront/src/store/cart.js
+++ storefront/src/store/cart.js
@@ -1,3 +1,4 @@
+import superagent from "superagent";
 let cart = [];
 
 export default (state = cart, action) => {
@@ -5,14 +6,35 @@ export default (state = cart, action) => {
     case "addToCart":
       return [...state, action.payload];
     case "del":
-      // should create module for cart 
+      // should create module for cart
       // should use superagent to delete from cart database(delete)
-      state.splice(action.payload,1)
+      state.splice(action.payload, 1);
       console.log(state);
-      return [...state]
+      return [...state];
 
     default:
       return state;
+  }
+};
+let url = "https://productsandstuff.herokuapp.com/api/v1/products";
+
+export const updateRemoteData = (product) => (dispatch) => {
+  // return a fucntion that will call superagent API
+  if (product.inventoryCount) {
+    console.log("product.inventoryCount :: ", product.inventoryCount);
+    return superagent
+      .get(`${url}/${product._id}`)
+      .then((DBproduct) =>
+      // console.log(DBproduct.body)
+        superagent
+          .put(`${url}/${DBproduct.body[0]._id}`)
+          .send({ ...DBproduct, inventoryCount: DBproduct.body.inventoryCount - 1 })
+      )
+      .then((data) => {
+        // call my action after getting the API response.
+        console.log(">>>>>>>>>>>>>>>>>>>>>>", data);
+        dispatch(updateCart(data.body));
+      });
   }
 };
 
